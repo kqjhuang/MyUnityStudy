33570ca51c2e44da778d8903d1b94f97ff5de30b
huangyimin <huangyimin@miyoo.cn>
2015-10-23 18:02:02 +0800

    import

---
+++ stone/Assets/UnityChan/Scripts/creat_cube.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creat_cube : MonoBehaviour {
+	
+	GameObject[] objs;
+
+	GameObject obj;
+	// Use this for initialization
+	void Start () {
+		int randomNum = Random.Range (1, 6);
+		objs = new GameObject[randomNum];
+		obj = Resources.Load("Cube") as GameObject;
+
+
+		for (int i = 0; i < randomNum; i++) {
+
+			objs[i] = Instantiate(obj,new Vector3(Random.Range(-5, 5),0.025f,Random.Range(-5, 5)),obj.transform.localRotation) as GameObject;
+			objs[i].transform.SetParent(transform,true);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ stone/Assets/UnityChan/Scripts/cube_show.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube_show : MonoBehaviour {
+
+	Transform role_pos;
+	int count;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		transform.localPosition = new Vector3(Random.Range(-5, 5),0.025f,Random.Range(-5, 5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+//		Destroy(other.gameObject);
+		count++;
+		Debug.Log (count);
+		transform.localPosition = new Vector3(Random.Range(-5, 5),0.025f,Random.Range(-5, 5));
+//		Destroy ();
+
+	}
+
+}
